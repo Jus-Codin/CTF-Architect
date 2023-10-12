3ab7a7008c0b12d2ba3a899e6be10f39fb0a219a
Jus-Codin <70018166+Jus-Codin@users.noreply.github.com>
2023-10-12 21:50:25 +0800

    Separate category ports

--- ctf_architect/core/mappings.py
+++ ctf_architect/core/mappings.py
@@ -12,15 +12,24 @@ def generate_port_mappings() -> dict[str, PortMappingDict]:
 
   mapping: dict[str, PortMappingDict] = {}
 
-  for challenge in walk_challenges():
-    if challenge.services is not None:
-      for service in challenge.services:
-        if service.name in mapping:
-          raise ValueError(f"Duplicate service name: {service.name}")
-        mapping[service.name] = {
-          "from_port": service.port,
-          "to_port": str(port)
-        }
-        port += 1
+  for category in config.categories:
+    for challenge in walk_challenges(category):
+      if challenge.services is not None:
+        for service in challenge.services:
+          if service.name in mapping:
+            raise ValueError(f"Duplicate service name: {service.name}")
+          mapping[service.name] = {
+            "from_port": service.port,
+            "to_port": str(port)
+          }
+          port += 1
+    # If there is at least 1 service, go to the next thousand
+    if port % 1000:
+      port += 1000 - (port % 1000)
+
+  # Check if we exceeded the port range
+  # I'm sure this won't happen but just in case
+  if port > 65535:
+    raise ValueError("Port range exceeded")
 
   return mapping
