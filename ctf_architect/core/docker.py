bae45d085703af1f442b6d792e604a421baf73b4
Jus-Codin <70018166+Jus-Codin@users.noreply.github.com>
2024-01-20 18:21:07 +0800

    Isolate challenge services in networks

--- ctf_architect/core/docker.py
+++ ctf_architect/core/docker.py
@@ -12,9 +12,22 @@ from ctf_architect.core.config import load_config
 from ctf_architect.core.models import Service
 
 
+def challenge_name_to_network_name(challenge_name: str) -> str:
+  """
+  Converts a challenge name to a network name
+
+  All non alphanumeric characters are removed, and spaces are replaced with dashes
+  """
+  return "".join(
+    c for c in challenge_name.lower().replace(" ", "-")
+    if c.isalnum()
+  )
+
+
 def create_compose_dict(
     service: Service,
     challenge_name: str,
+    network_name: str,
     category: str,
     host_port: str
   ) -> dict:
@@ -31,6 +44,7 @@ def create_compose_dict(
       "build": service_path,
       "ports": [f"{host_port}:{service.port}"],
       "container_name": service.name,
+      "networks": [network_name]
   }
 
   if service.extras is not None:
@@ -53,13 +67,24 @@ def create_compose_file() -> None:
     raise ValueError("Port mappings not found in config, please generate them first")
 
   services = {}
+  networks = {}
 
   for challenge in walk_challenges():
     if challenge.services is not None:
+
+      network_name = challenge_name_to_network_name(challenge.name)
+
+      if network_name not in networks:
+        networks[network_name] = {
+          "internal": True,
+          "name": network_name
+        }
+
       for service in challenge.services:
         services[service.name] = create_compose_dict(
           service,
           challenge.name,
+          network_name,
           challenge.category.lower(),
           config.port_mappings[service.name]["to_port"]
         )
@@ -67,5 +92,6 @@ def create_compose_file() -> None:
   with open("docker-compose.yml", "w") as f:
     safe_dump({
       "version": "3",
-      "services": services
+      "services": services,
+      "networks": networks
     }, f)
