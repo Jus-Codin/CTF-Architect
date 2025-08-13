cd7da150a636cc620467d48a2d76805b3caf72b4
Jus-Codin <70018166+Jus-Codin@users.noreply.github.com>
2025-08-13 10:37:45 +0800

    refactor: use Repo API

--- src/ctf_architect/core/port_mapping.py
+++ src/ctf_architect/core/port_mapping.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-import os
 from functools import lru_cache
+from pathlib import Path
 
 import yaml
 
@@ -10,24 +10,35 @@ from ctf_architect.core.exceptions import (
     DuplicateServiceNameError,
     MissingStartingPortError,
 )
-from ctf_architect.core.repo import load_repo_config, walk_challenges
+from ctf_architect.core.repo import Repo
 from ctf_architect.models.port_mapping import PortMapping, PortMappingFile
 
+PortMappingsDict = dict[str, list[PortMapping]]
 
+
+# TODO: Make the path variable mandatory to be specified to the function
 @lru_cache
-def load_port_mapping() -> dict[str, list[PortMapping]]:
+def load_port_mapping(path: str | Path = Path.cwd()) -> dict[str, list[PortMapping]]:
     """Load the port mapping from the port_mapping.yaml file.
+
+    If the path is a directory, it will look for the port_mapping.yaml file in that directory.
+    If the path is a file, it will load the port mapping from that file.
+
+    Args:
+        path (str | Path): The path to the port mapping file or directory. Defaults to the current working directory.
 
     Returns:
         dict[str, list[PortMapping]]: A dictionary of service names to port mappings.
-
-    Raises:
-        FileNotFoundError: If the port_mapping.yaml file is not found.
     """
-    if not os.path.exists(PORT_MAPPING_FILE):
-        raise FileNotFoundError(f"Could not find {PORT_MAPPING_FILE}")
+    if isinstance(path, str):
+        path = Path(path)
 
-    with open(PORT_MAPPING_FILE) as f:
+    if path.is_file():
+        fp = path
+    else:
+        fp = path / PORT_MAPPING_FILE
+
+    with open(fp) as f:
         data = yaml.safe_load(f)
 
     mapping_file = PortMappingFile.model_validate(data)
@@ -35,35 +46,42 @@ def load_port_mapping() -> dict[str, list[PortMapping]]:
     return mapping_file.mapping
 
 
-def save_port_mapping(mapping: dict[str, list[PortMapping]]) -> None:
-    """Save the port mapping to the port_mapping.yaml file."""
+def save_port_mapping(mapping: PortMappingsDict) -> None:
+    """Save the port mapping to the port_mapping.yaml file.
+
+    Args:
+        mapping (dict[str, list[PortMapping]]): A dictionary of service names to port mappings.
+    """
     data = PortMappingFile.from_mapping(mapping)
 
     with open(PORT_MAPPING_FILE, "w") as f:
         yaml.safe_dump(data.model_dump(), f)
 
 
-def generate_port_mapping(seperation: int | None = 1000, max_port: int = 65535) -> dict[str, list[PortMapping]]:
+def generate_port_mapping(
+    repo_path: str | Path = Path.cwd(), seperation: int | None = 1000, max_port: int = 65535
+) -> PortMappingsDict:
     """Generate a port mapping for services in the repository.
 
     Args:
+        repo_path (str | Path): The path to the repository. Defaults to the current working directory.
         seperation (int | None, optional): The number of ports to separate public services by. Defaults to 1000.
         max_port (int, optional): The maximum port number to use. Defaults to 65535.
 
     Returns:
-        dict[str, list[PortMapping]]: A dictionary of service names to port mappings.
+        PortMappingsDict: A dictionary of service names to port mappings.
 
     Raises:
         DuplicateServiceNameError: If there are multiple services with the same name.
         MissingStartingPortError: If no starting port is specified in the repo config.
         ValueError: If the starting port is greater than the max port or there are not enough ports to assign to all services.
     """
-    config = load_repo_config()
+    repo = Repo.from_path(repo_path)
 
-    if config.starting_port is None:
+    if repo.ctf_config.starting_port is None:
         raise MissingStartingPortError("No starting port specified in the repo config")
 
-    port = config.starting_port
+    port = repo.ctf_config.starting_port
 
     if port > max_port:
         raise ValueError("Starting port is greater than max port")
@@ -72,28 +90,27 @@ def generate_port_mapping(seperation: int | None = 1000, max_port: int = 65535) 
     # secret_services = []
 
     # There has to be a better way to do this...
-    for category in config.categories:
-        for challenge in walk_challenges(category):
-            if challenge.services is not None:
-                for service in challenge.services:
-                    service_name = service.unique_name(challenge)
+    for challenge in repo.walk_challenges():
+        if challenge.config.services is not None:
+            for service in challenge.config.services:
+                service_name = service.unique_name(challenge.config)
 
-                    if service_name in mapping:
-                        raise DuplicateServiceNameError(f"Duplicate service name: {service_name}")
+                if service_name in mapping:
+                    raise DuplicateServiceNameError(f"Duplicate service name: {service_name}")
 
-                    service_mapping = []
+                service_mapping = []
 
-                    for service_port in service.ports_list:
-                        # TODO: Create built-in solution for randomised ports for secret services
-                        # if service.type == "secret":
-                        #     secret_services.append((service_name, service))
-                        if service.type == "internal":
-                            service_mapping.append(PortMapping(from_port=service_port, to_port=None))
-                        else:
-                            service_mapping.append(PortMapping(from_port=service_port, to_port=port))
-                            port += 1
+                for service_port in service.ports_list:
+                    # TODO: Create built-in solution for randomised ports for secret services
+                    # if service.type == "secret":
+                    #     secret_services.append((service_name, service))
+                    if service.type == "internal":
+                        service_mapping.append(PortMapping(from_port=service_port, to_port=None))
+                    else:
+                        service_mapping.append(PortMapping(from_port=service_port, to_port=port))
+                        port += 1
 
-                    mapping[service_name] = service_mapping
+                mapping[service_name] = service_mapping
 
         if seperation is not None:
             # If there is at least one public service, add a separation between them
